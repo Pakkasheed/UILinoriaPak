d9732bc44170bb1664efc6dca49b9d3d2a36f5a7
Pakkasheed <111211044+Pakkasheed@users.noreply.github.com>
2022-12-14 22:08:59 +0700

    Update UIPakHub.lua

--- UIPakHub.lua
+++ UIPakHub.lua
@@ -31,7 +31,7 @@ local Library = {
     FontColor = Color3.fromRGB(255, 255, 255);
     MainColor = Color3.fromRGB(0, 0, 0);
     BackgroundColor = Color3.fromRGB(0, 0, 0);
-    AccentColor = Color3.fromRGB(255, 204, 153);
+    AccentColor = Color3.fromRGB(153, 255, 153);
     OutlineColor = Color3.fromRGB(21, 20, 18);
 
     Black = Color3.new(0, 0, 0);
